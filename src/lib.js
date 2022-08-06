af0678300f99c70da18472eb7c07f8f63ac9221b
seasky-coder <zhouj586@163.com>
2022-08-06 14:36:54 +0800

    add chinese

--- src/lib.js
+++ src/lib.js
@@ -25,9 +25,63 @@ module.exports.rpad = function rpad(str, len, char) {
  * @param {number} offset
  * @returns {number}
  */
-module.exports.writeField = function writeField(view, fieldLength, str, offset) {
+module.exports.writeField2 = function writeField(view, fieldLength, str, offset) {
+    debugger;
     for (var i = 0; i < fieldLength; i++) {
         view.setUint8(offset, str.charCodeAt(i)); offset++;
     }
     return offset;
 };
+
+module.exports.writeField = function writeField(view, fieldLength, str, offset) {
+    debugger;
+    console.log("writeField===1>",result);
+    for (var i = 0; i < fieldLength; i++) {
+        var index = 0;
+        for (var writeByte = 0; writeByte < str.length; ) {
+            var result = writeUTF(str[index]); // => [0, 3, 228, 184, 173]
+            //currentOffset += result[1];
+            console.log("writeField===2>",result);
+            for (var codeIndex = 0; offset < result[1]; codeIndex++) {
+                view.setUint8(offset, result[2 + codeIndex]);
+                offset += 1;
+                writeByte++;
+            }
+            index++;
+        }
+    }
+    return offset;
+};
+
+module.exports.writeUTF=function (str, isGetBytes) {
+    var back = [];
+    var byteSize = 0;
+    for (var i = 0; i < str.length; i++) {
+        var code = str.charCodeAt(i);
+        if (0x00 <= code && code <= 0x7f) {
+            byteSize += 1;
+            back.push(code);
+        } else if (0x80 <= code && code <= 0x7ff) {
+            byteSize += 2;
+            back.push((192 | (31 & (code >> 6))));
+            back.push((128 | (63 & code)))
+        } else if ((0x800 <= code && code <= 0xd7ff)
+            || (0xe000 <= code && code <= 0xffff)) {
+            byteSize += 3;
+            back.push((224 | (15 & (code >> 12))));
+            back.push((128 | (63 & (code >> 6))));
+            back.push((128 | (63 & code)))
+        }
+    }
+    for (i = 0; i < back.length; i++) {
+        back[i] &= 0xff;
+    }
+    if (isGetBytes) {
+        return back
+    }
+    if (byteSize <= 0xff) {
+        return [0, byteSize].concat(back);
+    } else {
+        return [byteSize >> 8, byteSize & 0xff].concat(back);
+    }
+}
