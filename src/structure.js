3939c8c8d69c62429ded82b5ee951e82d650f297
seasky-coder <zhouj586@163.com>
2022-08-07 13:25:10 +0800

    mod  field

--- src/structure.js
+++ src/structure.js
@@ -51,11 +51,13 @@ module.exports = function structure(data, meta) {
         var k=1;
         f.name.split('').forEach(function(c, x) {
             var result = lib.writeUTF(c);
-            for (var codeIndex = 2; codeIndex < result.length; codeIndex++) {
-                if(k>10)break;
-                mOffset+=k;
-                view.setUint8(mOffset, result[codeIndex]);
-                k++;
+            if (k + result[1] < 10) {
+                for (var codeIndex = 2; codeIndex < result.length; codeIndex++) {
+                    if (k > 10) break;
+                    mOffset += k;
+                    view.setUint8(mOffset, result[codeIndex]);
+                    k++;
+                }
             }
            // view.setInt8(32 + i * 32 + x, c.charCodeAt(0));
         });
@@ -70,43 +72,45 @@ module.exports = function structure(data, meta) {
     var offset = fieldDescLength + 32;
 
     data.forEach(function(row, num) {
-        // delete flag: this is not deleted
-                view.setUint8(offset, 32);
-        offset++;
-        field_meta.forEach(function(f) {
-            var val = row[f.name];
-            if (val === null || typeof val === 'undefined') val = '';
+        if(offset<buffer-2) {
+            // delete flag: this is not deleted
+            view.setUint8(offset, 32);
+            offset++;
+            field_meta.forEach(function (f) {
+                var val = row[f.name];
+                if (val === null || typeof val === 'undefined') val = '';
 
-            switch (f.type) {
-                // boolean
-                case 'L':
-                    view.setUint8(offset, val ? 84 : 70);
-                    offset++;
-                    break;
+                switch (f.type) {
+                    // boolean
+                    case 'L':
+                        view.setUint8(offset, val ? 84 : 70);
+                        offset++;
+                        break;
 
-                // date
-                case 'D':
-                    offset = lib.writeField(view, 8,
-                        lib.lpad(val.toString(), 8, ' '), offset);
-                    break;
+                    // date
+                    case 'D':
+                        offset = lib.writeField(view, 8,
+                            lib.lpad(val.toString(), 8, ' '), offset);
+                        break;
 
-                // number
-                case 'N':
-                    offset = lib.writeField(view, f.size,
-                        lib.lpad(val.toString(), f.size, ' ').substr(0, 18),
-                        offset);
-                    break;
+                    // number
+                    case 'N':
+                        offset = lib.writeField(view, f.size,
+                            lib.lpad(val.toString(), f.size, ' ').substr(0, 18),
+                            offset);
+                        break;
 
-                // string
-                case 'C':
-                    offset = lib.writeField(view, f.size,
-                        lib.rpad(val.toString(), f.size, ' '), offset);
-                    break;
+                    // string
+                    case 'C':
+                        offset = lib.writeField(view, f.size,
+                            lib.rpad(val.toString(), f.size, ' '), offset);
+                        break;
 
-                default:
-                    throw new Error('Unknown field type');
-            }
-        });
+                    default:
+                        throw new Error('Unknown field type');
+                }
+            });
+        }
     });
 
     // EOF flag
